5042b772da2d7acfbab2912a2a0492fa988cb4ce
Connor Burge <connor.d.burge@gmail.com>
2025-10-05 16:12:16 -0400

    wip

--- src/main/java/com/mochafund/transactionsservice/common/events/EventType.java
+++ src/main/java/com/mochafund/transactionsservice/common/events/EventType.java
@@ -5,19 +5,15 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class EventType {
     public static final String WORKSPACE_CREATED = "workspace.created";
-    public static final String WORKSPACE_UPDATED = "workspace.updated";
     public static final String WORKSPACE_DELETED = "workspace.deleted";
 
     public static final String TAG_CREATED = "tag.created";
-    public static final String TAG_UPDATED = "tag.updated";
     public static final String TAG_DELETED = "tag.deleted";
 
     public static final String ACCOUNT_CREATED = "account.created";
-    public static final String ACCOUNT_UPDATED = "account.updated";
     public static final String ACCOUNT_DELETED = "account.deleted";
 
     public static final String CATEGORY_CREATED = "category.created";
-    public static final String CATEGORY_UPDATED = "category.updated";
     public static final String CATEGORY_DELETED = "category.deleted";
 
     public static final String TRANSACTION_CREATED = "transaction.created";
@@ -25,6 +21,5 @@ public class EventType {
     public static final String TRANSACTION_DELETED = "transaction.deleted";
 
     public static final String MERCHANT_CREATED = "merchant.created";
-    public static final String MERCHANT_UPDATED = "merchant.updated";
     public static final String MERCHANT_DELETED = "merchant.deleted";
 }
